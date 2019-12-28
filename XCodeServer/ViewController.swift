4150d4b7afc19d58dbe2928b6d664cd338c283de
Asep Abdaz <asepabdaz@gmail.com>
2019-12-28 14:53:38 +0700

    Initial Commit

---
+++ XCodeServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCodeServer
+//
+//  Created by Asep Abdaz on 28/12/19.
+//  Copyright © 2019 Asep Abdaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
